485a48db5e0badf3adf6a2ca8d3c03d1964892a7
Silvio Malisani <silvio.malisani@webformat.com>
2017-06-04 11:13:05 +0200

    La matrice $res[$k1][$k2] viene construita a partire dallo stack dei tempi: tiene traccia del totale dei tempi e del numero di volte in cui si passa dalla chiave $k1 alla chiave $k2

--- lib/Webformat/Profiler/Simple.php
+++ lib/Webformat/Profiler/Simple.php
@@ -11,6 +11,10 @@ class Webformat_Profiler_Simple extends Webformat_Profiler_Abstract
         list($k1,$t1) = array_shift($this->times);
         $keys = array($k1);
         foreach($this->times as list($k2,$t2)){
+            if(!array_key_exists($k1,$res))
+                $res[$k1] = array($k2 => array(0,0));
+            if(!array_key_exists($k2,$res[$k1]))
+                $res[$k1][$k2] = array(0,0);
         }
         file_put_contents($filename,$file);
     }
