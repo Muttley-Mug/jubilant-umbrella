5a6423d4ce77ba6cd3776a45d8230ba52fbcddd5
Silvio Malisani <silvio.malisani@webformat.com>
2017-06-04 10:46:25 +0200

    La struttura base per la profilazione è l'array times e la funzione profile. profile accetta una chiave (opzionale) e salva <chiave,timestamp> nello stack times Questo viene implementato qui perchè non c'è necessità di cambiarlo nelle classi derivate.

--- lib/Webformat/Profiler/Abstract.php
+++ lib/Webformat/Profiler/Abstract.php
@@ -2,4 +2,20 @@
 
 abstract class Webformat_Profiler_Abstract
 {
+    protected $times = null;
+
+
+    public function profile($key = null)
+    {
+        if(!$this->enabled) return;
+        if(is_null($key)){
+            $key = $this->defaultKey();
+        }
+        if($this->doNotProfile($key)) return;
+        if(is_null($this->times))
+            $this->times=array(array($key,microtime(true)));
+        else
+            $this->times[] = array($key,microtime(true));
+    }
+
 }
